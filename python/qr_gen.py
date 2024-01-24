d4be12f2cadccb05b7cb73284e23b5e7af76ab87
Annika Stein <annika-stein@t-online.de>
2024-01-24 21:40:38 +0100

    feat: svg QR code (for trifold brochure)

--- python/qr_gen.py
+++ python/qr_gen.py
@@ -1,5 +1,5 @@
 import qrcode
-
+import qrcode.image.svg
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers.pil import SquareModuleDrawer, RoundedModuleDrawer
 from qrcode.image.styles.colormasks import RadialGradiantColorMask
@@ -12,6 +12,13 @@ qr = qrcode.QRCode(
 )
 qr.add_data('https://annikastein.github.io/my-comp/')
 
+# ========== SVG part ==========
+# Combined path factory, fixes white space that may occur when zooming
+factory = qrcode.image.svg.SvgPathImage
+img = qr.make_image(image_factory=factory)
+img.save("figures/simple_qr.svg")
+
+# ========== PNG part ==========
 img_0 = qr.make_image(image_factory=StyledPilImage, module_drawer=SquareModuleDrawer())
 img_1 = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer())
 img_2 = qr.make_image(image_factory=StyledPilImage, color_mask=RadialGradiantColorMask())
